037911aa18677660e415ad6579fa0b64f8e447f8
Abdiel <abdielivan02@gmail.com>
2021-02-13 10:08:31 -0500

    Creación de la clase BaseObject

--- tmx/base/__init__.py
+++ tmx/base/__init__.py
@@ -20,6 +20,11 @@ class BaseLayer:
         except ValueError: return float(self._data.attrib.get("offsetx"))
 
     @property
+    def opacity(self) -> Union[int, float]:
+        try: return int(self._data.attrib.get("opacity", 1))
+        except ValueError: return float(self._data.attrib.get("opacity"))
+
+    @property
     def offsety(self) -> Union[int, float]:
         try: return int(self._data.attrib.get("offsety", 0))
         except ValueError: return float(self._data.attrib.get("offsety"))
@@ -32,11 +37,6 @@ class BaseLayer:
         } if self._data.find("properties") != None else None
 
     @property
-    def opacity(self) -> Union[int, float]:
-        try: return int(self._data.attrib.get("opacity", 1))
-        except ValueError: return float(self._data.attrib.get("opacity"))
-
-    @property
     def tintcolor(self) -> Optional[Color]:
         return Color(self._data.attrib.get("tintcolor")) \
             if self._data.attrib.get("tintcolor", None) != None \
@@ -44,4 +44,47 @@ class BaseLayer:
 
     @property
     def visible(self) -> bool:
-        return bool(int(self._data.attrib.get("visible", 1)))  
+        return bool(int(self._data.attrib.get("visible", 1)))
+
+class BaseObject:
+    def __init__(self, data: ElementTree.Element):
+        self._data = data
+
+    @property
+    def id(self) -> int:
+        return int(self._data.attrib.get("id"))
+
+    @property
+    def name(self) -> Optional[str]:
+        return self._data.attrib.get("name", None)
+
+    @property
+    def properties(self) -> Optional[Dict[str, str]]:
+        return {
+            prop.attrib.get("name"):prop.attrib.get("value")
+            for prop in self._data.find("properties").iter("property")
+        } if self._data.find("properties") != None else None
+    
+    @property
+    def rotation(self) -> Union[int, float]:
+        try: return int(self._data.attrib.get("rotation"))
+        except ValueError: return float(self._data.attrib.get("rotation"))
+        except TypeError: return 0
+
+    @property
+    def type(self) -> Optional[str]:
+        return self._data.attrib.get("type", None)
+
+    @property
+    def visible(self) -> bool:
+        return bool(self._data.attrib.get("visible", 1))
+
+    @property
+    def x(self) -> Union[int, float]:
+        try: return int(self._data.attrib.get("x"))
+        except ValueError: return float(self._data.attrib.get("x"))
+
+    @property
+    def y(self) -> Union[int, float]:
+        try: return int(self._data.attrib.get("y"))
+        except ValueError: return float(self._data.attrib.get("y"))
