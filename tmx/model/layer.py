68ef07a718603663d5395bc04f03c1149bfeb0b5
Abdiel <abdielivan02@gmail.com>
2021-02-13 10:16:23 -0500

    Creación de la clase ObjectGroup

--- tmx/model/layer.py
+++ tmx/model/layer.py
@@ -2,6 +2,7 @@ from xml.etree import ElementTree
 from typing import Optional, Tuple, Union
 from tmx.const import EncodingFormat
 from tmx.base import BaseLayer
+from .extras import Color
 
 class Data:
     def __init__(self, data: ElementTree.Element):
@@ -37,3 +38,30 @@ class Layer(BaseLayer):
     @property
     def width(self) -> int:
         return int(self._data.attrib.get("width"))
+
+class ObjectGroup(BaseLayer):
+    def __init__(self, data: ElementTree.Element) -> None:
+        super().__init__(data)
+
+    @property
+    def color(self) -> Optional[Color]:
+        return Color(self._data.attrib.get("color")) \
+            if self._data.attrib.get("color", None) \
+            else None
+
+    @property
+    def draworder(self) -> str:
+        return self._data.attrib.get("draworder", "topdown")
+
+    @property
+    def height(self) -> Union[int, float]:
+        try: return int(self._child.attrib.get("height"))
+        except ValueError: return float(self._child.attrib.get("height"))
+
+    @property
+    def width(self) -> Union[int, float]:
+        try: return int(self._child.attrib.get("width"))
+        except ValueError: return float(self._child.attrib.get("width"))
+
+    def get_objects(self) -> None:
+        pass
