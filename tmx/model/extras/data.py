b6aac2aa59a24fb0ffc770bb3a8e3a3ff2c5d22e
Abdiel <abdielivan02@gmail.com>
2021-02-13 10:28:43 -0500

    Se movió la clase Data hacia el módulo extras
    Corrección en las importaciones en el módulo layer

---
+++ tmx/model/extras/data.py
@@ -0,0 +1,22 @@
+from xml.etree import ElementTree
+from typing import Union
+from tmx.const import EncodingFormat
+
+class Data:
+    def __init__(self, data: ElementTree.Element):
+        self._data = data
+
+    @property
+    def encoding(self) -> str:
+        return self._data.attrib.get("encoding")
+
+    @property
+    def compression(self) -> str:
+        return self._data.attrib.get("compression")
+
+    @property
+    def value(self) -> Union[bytes, str]:
+        if self.encoding == EncodingFormat.BASE64:
+            return bytes(self._data.text.strip(), "UTF-8")
+        else:
+            return self._data.text.strip().replace("\n", "")
--- tmx/model/layer.py
+++ tmx/model/layer.py
@@ -1,27 +1,8 @@
 from xml.etree import ElementTree
 from typing import Optional, Tuple, Union
-from tmx.const import EncodingFormat
 from tmx.base import BaseLayer
 from .extras import Color
-
-class Data:
-    def __init__(self, data: ElementTree.Element):
-        self._data = data
-
-    @property
-    def encoding(self) -> str:
-        return self._data.attrib.get("encoding")
-
-    @property
-    def compression(self) -> str:
-        return self._data.attrib.get("compression")
-
-    @property
-    def value(self) -> Union[bytes, str]:
-        if self.encoding == EncodingFormat.BASE64:
-            return bytes(self._data.text.strip(), "UTF-8")
-        else:
-            return self._data.text.strip().replace("\n", "")
+from .extras.data import Data
 
 class Layer(BaseLayer):
     def __init__(self, data: ElementTree.Element) -> None:
